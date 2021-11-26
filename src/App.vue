7e2a480178d846a997948582dd234b5ce41ab34c
CyanLumi <ajyeomans22@gmail.com>
2021-11-26 18:08:43 +1100

    ToDoItem component addition

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <div id="app">
     <h1>To-Do List</h1>
+    <ul>
+      <li>
+        <to-do-item label="My ToDo Item" :done="true"></to-do-item>
+      </li>
+    </ul>
   </div>
+  
 </template>
 
 <script>
+import ToDoItem from './components/ToDoItem.vue';
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    ToDoItem
+  }
 }
 </script>
 
