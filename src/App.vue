1df4cdb71120935d797507bdc2d3c5d1b6f33dde
CyanLumi <ajyeomans22@gmail.com>
2021-11-28 12:33:41 +1100

    Editing todo form component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
-        <div id="app">
-                <h1>To-Do List</h1>
-                <to-do-form @todo-added="addToDo"></to-do-form>
-                <h2 id="list-summary">{{listSummary}}</h2>
-                <ul aria-labelledby="list-summary" class="stack-large">
-                        <li v-for="item in ToDoItems" :key="item.id">
-                                <to-do-item :label="item.label" :done="item.done" :id="item.id" @checkbox-changed="updateDoneStatus(item.id)"></to-do-item>
-                        </li>
-                </ul>
-        </div>
+    <div id="app">
+        <h1>To-Do List</h1>
+        <to-do-form @todo-added="addToDo"></to-do-form>
+        <h2 id="list-summary">{{listSummary}}</h2>
+        <ul aria-labelledby="list-summary" class="stack-large">
+            <li v-for="item in ToDoItems" :key="item.id">
+                <to-do-item :label="item.label" :done="item.done" :id="item.id" 
+                                @checkbox-changed="updateDoneStatus(item.id)"
+                                @item-deleted="deleteToDo(item.id)"
+                                @item-edited="editToDo(item.id, $event)">
+                </to-do-item>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -17,37 +21,45 @@ import ToDoForm from "./components/ToDoForm.vue";
 import uniqueId from 'lodash.uniqueid';
 
 export default {
-        name: "App",
-        components: {
-                ToDoItem,
-                ToDoForm
+    name: "App",
+    components: {
+        ToDoItem,
+        ToDoForm
+    },
+    data() {
+        return {
+            // ToDoItems: [
+            //     { id: uniqueId('todo-'), label: 'Learn Vue', done: false },
+            //     { id: uniqueId('todo-'), label: 'Create a Vue project with the CLI', done: true },
+            //     { id: uniqueId('todo-'), label: 'Have fun', done: true },
+            //     { id: uniqueId('todo-'), label: 'Create a to-do list', done: false }
+            // ]
+            ToDoItems: []
+        };
+    },
+    methods: {
+        addToDo(toDoLabel) {
+            this.ToDoItems.push({id:uniqueId('todo-'), label: toDoLabel, done: false})
         },
-        data() {
-                return {
-                        // ToDoItems: [
-                        //         { id: uniqueId('todo-'), label: 'Learn Vue', done: false },
-                        //         { id: uniqueId('todo-'), label: 'Create a Vue project with the CLI', done: true },
-                        //         { id: uniqueId('todo-'), label: 'Have fun', done: true },
-                        //         { id: uniqueId('todo-'), label: 'Create a to-do list', done: false }
-                        // ]
-                        ToDoItems: []
-                };
+        updateDoneStatus(toDoId) {
+            const toDoToUpdate = this.ToDoItems.find(item => item.id === toDoId)
+            toDoToUpdate.done = !toDoToUpdate.done
         },
-        methods: {
-                addToDo(toDoLabel) {
-                        this.ToDoItems.push({id:uniqueId('todo-'), label: toDoLabel, done: false})
-                },
-                updateDoneStatus(toDoId) {
-                    const toDoToUpdate = this.ToDoItems.find(item => item.id === toDoId)
-                    toDoToUpdate.done = !toDoToUpdate.done
-                }
+        deleteToDo(toDoId) {
+            const itemIndex = this.ToDoItems.findIndex(item => item.id === toDoId);
+            this.ToDoItems.splice(itemIndex, 1);
         },
-        computed: {
-            listSummary() {
-                const numberFinishedItems = this.ToDoItems.filter(item => item.done).length
-                return `${numberFinishedItems} out of ${this.ToDoItems.length} items completed`
-            }
+        editToDo(toDoId, newLabel) {
+            const toDoToEdit = this.ToDoItems.find(item => item.id ===toDoId);
+            toDoToEdit.label = newLabel;
         }
+    },
+    computed: {
+        listSummary() {
+        const numberFinishedItems = this.ToDoItems.filter(item => item.done).length
+        return `${numberFinishedItems} out of ${this.ToDoItems.length} items completed`
+        }
+    }
 };
 </script>
 
@@ -99,7 +111,7 @@ export default {
 }
 @media screen and (min-width: 620px) {
     [class*="__lg"] {
-        font-size: 2.4rem;
+    font-size: 2.4rem;
     }
 }
 .visually-hidden {
@@ -124,10 +136,10 @@ export default {
 }
 @media screen and (min-width: 550px) {
     .stack-small > * + * {
-        margin-top: 1.4rem;
+    margin-top: 1.4rem;
     }
     .stack-large > * + * {
-        margin-top: 2.8rem;
+    margin-top: 2.8rem;
     }
 }
 /* End global styles */
@@ -141,7 +153,7 @@ export default {
 }
 @media screen and (min-width: 550px) {
     #app {
-        padding: 4rem;
+    padding: 4rem;
     }
 }
 #app > * {
